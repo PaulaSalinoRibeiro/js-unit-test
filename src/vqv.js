e1f1f5c3931136522e87bf3516b53f052fb0b142
PAULA RIBEIRO <paulasalinor@gmail.com>
2022-01-17 15:51:44 -0300

    Requisito 3 ok

--- src/vqv.js
+++ src/vqv.js
@@ -16,6 +16,15 @@
       #VQV!'
 */
 
-const vqv = () => {};
+const vqv = (str, num) => {
+  if (!str && !num) {
+    return undefined;
+  }
+
+  return `Oi, meu nome é ${str}!
+Tenho ${num} anos,
+trabalho na Trybe e mando muito em programação!
+#VQV!`;
+};
 
 module.exports = vqv;
