1a5aaf093739d634ba194facabadbcf0f2d5798a
PAULA RIBEIRO <paulasalinor@gmail.com>
2022-01-17 15:19:48 -0300

    Requisito 1 ok

--- src/average.js
+++ src/average.js
@@ -12,8 +12,23 @@
     - average([1, '2']) // Retorno: undefined;
 */
 
-const average = () => {
-  // implementação aqui
+const average = (arr) => {
+  let sum = 0;
+
+  if (arr.length === 0) {
+    return undefined;
+  }
+
+  for (let i = 0; i < arr.length; i += 1) {
+    if (typeof arr[i] !== 'number') {
+      return undefined;
+    }
+    
+    sum += arr[i];  
+  }
+
+  let endAverage = Math.round(sum / arr.length);
+  return endAverage; 
 };
 
 module.exports = average;
